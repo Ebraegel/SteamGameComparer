fad05b4b152d8fbb842fe5b13623fefda567b603
Ebraegel <Ebraegelmann@gmail.com>
2014-01-15 22:47:35 -0600

    Comments in steam.rb

--- steam.rb
+++ steam.rb
@@ -5,7 +5,8 @@ require_relative './configuration.rb'
 #Currently, configuration.rb just holds the API key.  Sample:
 # API_KEY = "CNSEF8434NCDbunchacharactersSVLSNR"
 
-
+#Grab all extant Steam games and makes an array of hashes 
+#where keys are game ID numbers and values are the game titles as strings
 gamelist = open('http://api.steampowered.com/ISteamApps/GetAppList/v0001/')
 all_games_json = JSON.parse(gamelist.read)
 all_games = all_games_json["applist"]["apps"]["app"]
@@ -23,14 +24,14 @@ class Person
     @games = Array[]
   end
 
-  def get_games
+  def get_games #Pulls down owned games from the Steam API and makes an array of game ID numbers
     conn = open("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{API_KEY}&steamid=#{@steamid}&format=json")
     json_hash = JSON.parse(conn.read)
     games_hash = json_hash["response"]["games"]
     @games = games_hash.map { |hash| hash["appid"] }
   end
 
-  def self.all
+  def self.all #testing
   ObjectSpace.each_object(self).each { |object| object.get_games}
   end
 
